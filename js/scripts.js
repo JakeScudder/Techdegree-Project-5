3090ab2af11a08c53f3bd938a20cc7d2f27d6de8
Jake M <scudsterr@gmail.com>
2019-09-25 11:39:55 -0400

    Styling
    Added fonts and styling

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ Fetch Functions
 ****************************************/
 
 //Initial fetch requests that pulls 12 random employees and then generates the cards and modals
-fetch('https://randomuser.me/api/?results=12&hl=en')
+fetch('https://randomuser.me/api/?results=12&nat=us')
     .then(response => response.json())
     .then(data => data.results.map(employee => {
         generateCard(employee);
@@ -70,7 +70,6 @@ function generateModal(data) {
                 <hr>
                 <p class="modal-text">${data.phone}</p>
                 <p class="modal-text">${data.location.street}, ${data.location.city}, ${data.location.state} ${data.location.postcode}</p>
-                <p class="modal-text">Birthday: ${data.dob.date}</p>
             </div>
         </div>
         <div id="${data.name.first}-btn" class="modal-btn-container" style="display: none">
@@ -82,25 +81,26 @@ function generateModal(data) {
     $('body').append(modal);
 }
 
+//function createSearchBar sets up the html for the search bar and appends it to the search container.
 function createSearchBar() {
     const search = `
-    <form action="#" method="get">
+    <form class="animated fadeIn" action="#" method="get">
         <input type="search" id="search-input" class="search-input" placeholder="Search...">
         <input onclick="searchFunction()" type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
     </form>
     `;
     $('.search-container').append(search);
 }
+//A timeout is used to create the search bar after the cards have been generated.
+setTimeout(createSearchBar, 1000)
 
+//When the submit button is clicked, searchFunction selects all the employee cards and then uses the .forEach method to search each card.  If that card includes the letters in the search bar, the modal with that ID will be put on the screen.
 function searchFunction(){
-    console.log(event.target.previousElementSibling.value);
     let names = document.querySelectorAll('.card');
     names.forEach(name => {
         if (name.lastElementChild.firstElementChild.textContent.includes(event.target.previousElementSibling.value.toLowerCase())) {
-
             let wholeName = name.lastElementChild.firstElementChild.textContent;
             let first = wholeName.substr(0, wholeName.indexOf(" "))
-            console.log(first);
             let searchModal = document.getElementById(first);
             let buttonID = `${first}-btn`;
             let modalButton = document.getElementById(buttonID)
@@ -108,11 +108,14 @@ function searchFunction(){
             searchModal.style.display = 'block';
             searchModal.parentElement.style.display = '';
             modalButton.style.display = 'block';
+
         }
     })
+    event.target.previousElementSibling.value = '';
 
 }
 
+//function prevButton finds the previousElementSibling of the event and brings that modal to the screen.
 function prevButton () {
         let prevModal = event.currentTarget.parentElement.parentElement.previousElementSibling
         dismiss();
@@ -123,6 +126,7 @@ function prevButton () {
 
 }
 
+//function nextButton finds the nextElementSibling of the event and brings that modal to the screen.
 function nextButton () {
     let nextModal = event.currentTarget.parentElement.parentElement.nextElementSibling
     console.log(nextModal);
@@ -133,24 +137,23 @@ function nextButton () {
     nextModal.lastElementChild.style.display = 'block';
 
 }
-setTimeout(disableButtons, 2000)
 
+//This function disables the first previous button and the last next button regardless of how many cards have been generated.
 function disableButtons () {
     let modalContainers = document.querySelectorAll('div.inactive-modal');
     let modalContainersLastElement = modalContainers.length - 1;
     let firstPrevButton = modalContainers[0].lastElementChild.firstElementChild;
     let lastNextButton = modalContainers[modalContainersLastElement].lastElementChild.lastElementChild;
-
     firstPrevButton.disabled = true;
     lastNextButton.disabled = true;
 
 }
 
+setTimeout(disableButtons, 2000)
 
 //function dismiss finds all the modals containers and modals in the body. It uses a for loop to re-assign their style.display to 'none';
 function dismiss(){
     let modalContainers = document.querySelectorAll('div.modal-container');
-
     for (let i = 0; i < modalContainers.length; i++) {
         modalContainers[i].className = 'inactive-modal';
         modalContainers[i].style.display = 'none';
@@ -167,8 +170,7 @@ Event Listeners
 
 //A brief timeout function to make sure the fetch request is completed before the addListener function assigns event listeners to each card.
 setTimeout(addListener, 1500)
-setTimeout(createSearchBar, 1000)
-// setTimeout(searchListener, 2000)
+
 
 //function addListener selects all elements with the class name 'card' and runs a for loop to assign eventListeners to each card.
 function addListener() {
@@ -188,11 +190,3 @@ function addListener() {
         });
     }
 }
-
-// function searchListener( ) {
-//     const submitButton = document.querySelector('#search-submit');
-//     submitButton.addEventListener('submit', function(event) {
-//         console.log('click');
-//         searchFunction(event);
-//     })
-// }
